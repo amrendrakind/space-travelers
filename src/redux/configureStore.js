d9a79c3f5da0497b249b0c6689eebbed261d2e6b
Ol-create <oluola96@gmail.com>
2022-05-17 03:37:22 -0600

    feat: add all reducers into one place

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,3 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rockets from './dragons/dragon'
+
+//Combine all the reducers into one place
+const reducer = combineReducers({
+	rockets,
+	missions,
+	dragons,
+  });
