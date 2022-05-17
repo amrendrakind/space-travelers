a2dcb199a27244c0e7589821663e5675eea211cf
Ol-create <oluola96@gmail.com>
2022-05-17 05:49:29 -0600

    feat: render dragon info with list element

--- src/components/dragons.js
+++ src/components/dragons.js
@@ -10,3 +10,23 @@ const Rockets = () => {
 	useEffect(() => {
 	  dispatch(getDragons());
 	}, []);
+
+	return (
+		<section>
+		  <ul className="dragonsList">
+			{dragonsList.map((dragon) => (
+			  <DragonsElement
+				key={dragon.dragon_id}
+				id={dragon.dragon_id}
+				name={dragon.dragon_name}
+				description={dragon.description}
+				flickrImages={dragon.flickr_images[0]}
+				reserved={dragon.reserved}
+			  />
+			))}
+		  </ul>
+		</section>
+	  );
+	};
+
+	export default Dragons;
