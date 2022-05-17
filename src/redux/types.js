69493e9b79d4985994a174c941627a13dc4b5c62
amrendrakind <vision2010@gmail.com>
2022-05-18 01:19:40 +0530

    Add Rockets Types

---
+++ src/redux/types.js
@@ -0,0 +1,3 @@
+export const GET_ROCKET_DATA = 'spacetravelers/rockets/GET_ROCKET_DATA';
+export const RESERVE_ROCKETS = 'spacetravelers/rockets/RESERVE_ROCKETS';
+export const CANCEL_RESERVATION = 'spacetravelers/rockets/CANCEL_RESERVATION';
