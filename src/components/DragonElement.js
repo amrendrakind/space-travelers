8eb0f1deecfe5986a3b933561ea726dca8cd2e46
Ol-create <oluola96@gmail.com>
2022-05-17 06:02:37 -0600

    feat: declare DragonElement Props

--- src/components/DragonElement.js
+++ src/components/DragonElement.js
@@ -2,3 +2,13 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { reserveDragon, unreserveDragon } from '../redux/dragons/dragon';
+
+const DragonElement = (props) => {
+	const dispatch = useDispatch();
+	const {
+	  id, name, dragon_type, flickrImages, reserved,
+	} = props;
+
+	DragonElement.defaultProps = {
+	  reserved: false,
+	};
