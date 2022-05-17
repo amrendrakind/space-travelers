c3d550869e1e2e368deda57d09d124fec751f9a4
Ol-create <oluola96@gmail.com>
2022-05-17 03:11:58 -0600

    feat: create getDragon function

--- src/redux/dragons/dragon.js
+++ src/redux/dragons/dragon.js
@@ -5,3 +5,14 @@ const RESERVEDRAGON = 'RESERVEDRAGON';
 const UNRESERVEDRAGON = 'UNRESERVEDRAGON';
 
 const initialState = [];
+
+let store = false;
+export const getDragons = () => async (dispatch) => {
+  const newState = await fetchDragons();
+  if (store) return;
+  dispatch({
+    type: FETCHDRAGON,
+    newState,
+  });
+  store = true;
+};
