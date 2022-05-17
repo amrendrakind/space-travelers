da904fb180fe26fcb213731dcd03515e2dc3dd83
Ol-create <oluola96@gmail.com>
2022-05-17 02:51:18 -0600

    feat: create fetchDragon function to call api

--- src/redux/dragons/api.js
+++ src/redux/dragons/api.js
@@ -1,2 +1,9 @@
-const apiDrangons = 'https://api.spacexdata.com/v3/dragons';
+const apiDragons = 'https://api.spacexdata.com/v3/dragons';
 
+const fetchDragons = async () => {
+	const get = await fetch(apiDragons);
+	const dragons = await get.json();
+	return dragons;
+  };
+
+  export default fetchDragons;
