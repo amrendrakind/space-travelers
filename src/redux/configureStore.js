80c12e7a193e117eab6d832f52fade745f3d4f2c
amrendrakind <vision2010@gmail.com>
2022-05-17 22:21:32 +0530

    Folder set up for redux and configureStore

---
+++ src/redux/configureStore.js
@@ -0,0 +1,17 @@
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rockets from '../rockets/rockets';
+
+// Combine all the reducers into one place
+const rootReducer = combineReducers({
+  rockets,
+  // missions,
+//   dragons,
+});
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk),
+);
+
+export default store;
