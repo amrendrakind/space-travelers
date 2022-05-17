ba03c27654011e7d9f17bed39f32e43c86ee3a90
Ol-create <oluola96@gmail.com>
2022-05-17 05:38:23 -0600

    feat: dispatch getDragons action

--- src/components/dragons.js
+++ src/components/dragons.js
@@ -3,3 +3,10 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getRockets } from '../redux/dragons/dragon';
 //import DragonElement from './DragonElement';
 //import './dragons.css';
+
+const Rockets = () => {
+	const dispatch = useDispatch();
+	const dragonsList = useSelector((store) => store.dragons);
+	useEffect(() => {
+	  dispatch(getDragons());
+	}, []);
