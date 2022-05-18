0118f085b1b7f003343a9ea5419b18eb2aca6f75
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-05-18 21:52:33 +0430

    Add Profile container

---
+++ src/components/pages/ProfileContainer.js
@@ -0,0 +1,9 @@
+import ProfileMissions from '../MissionsComponents/ProfileMissions';
+
+const ProfileContainer = () => (
+  <div className="profileContainer">
+    <ProfileMissions />
+  </div>
+);
+
+export default ProfileContainer;
