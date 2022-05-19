ca55bacf441975c831bb3cc2e4aece27ac773f31
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-05-19 20:45:32 +0430

    Fix eslint error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/configureStore';
