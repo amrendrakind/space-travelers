5314527432ba4da7270280109f3ea8899e9243f6
Amrendra K <amrendrak.ind@gmail.com>
2022-05-20 02:11:48 +0530

    Linter error fixed

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Rockets from './components/rockets/Rockets';
 import NavBar from './components/NavBar/NavBar';
 import ProfileContainer from './components/pages/ProfileContainer';
 
-import Profile from './components/pages/ProfileDragon';
-
 function App() {
   return (
     <>
