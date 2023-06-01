f9e6846bdd1778a97ceb2449086e0b32a191a328
Amrendra K <amrendrak.ind@gmail.com>
2023-06-01 13:15:55 +0100

    Update MissionItem.js

--- src/components/missions/MissionItem.js
+++ src/components/missions/MissionItem.js
@@ -3,45 +3,43 @@ import PropTypes from 'prop-types';
 import Badge from 'react-bootstrap/Badge';
 import JoinButton from './JoinButton';
 
-const MissionItem = (props) => {
-  const { mission } = props;
-  const rederingBage = () => {
-    if (mission.isJoined) {
-      return <Badge bg="success">Active Member</Badge>;
-    }
-    return <Badge bg="secondary">Not a Member</Badge>;
+const MissionItem = ({ mission }) => {
+  const { mission_name, mission_description, wikipedia, isJoined } = mission;
+
+  const renderBadge = () => {
+    return isJoined ? (
+      <Badge bg="success">Active Member</Badge>
+    ) : (
+      <Badge bg="secondary">Not a Member</Badge>
+    );
   };
 
   return (
     <tr>
-      <th>
-        <h3>{mission.mission_name}</h3>
-      </th>
-      <th>
-        <p>{mission.mission_description}</p>
-        <a
-          href={mission.wikipedia}
-          target="_blank"
-          rel="noreferrer"
-        >
+      <td>
+        <h3>{mission_name}</h3>
+      </td>
+      <td>
+        <p>{mission_description}</p>
+        <a href={wikipedia} target="_blank" rel="noreferrer">
           Read more
         </a>
-      </th>
-      <th className="align-middle">{rederingBage()}</th>
-      <th className="align-middle">
-        <JoinButton isJoined={mission.isJoined} id={mission.mission_id} />
-      </th>
+      </td>
+      <td className="align-middle">{renderBadge()}</td>
+      <td className="align-middle">
+        <JoinButton isJoined={isJoined} id={mission.mission_id} />
+      </td>
     </tr>
   );
 };
 
 MissionItem.propTypes = {
   mission: PropTypes.shape({
-    mission_id: PropTypes.string,
-    mission_name: PropTypes.string,
-    mission_description: PropTypes.string,
-    wikipedia: PropTypes.string,
-    isJoined: PropTypes.bool,
+    mission_id: PropTypes.string.isRequired,
+    mission_name: PropTypes.string.isRequired,
+    mission_description: PropTypes.string.isRequired,
+    wikipedia: PropTypes.string.isRequired,
+    isJoined: PropTypes.bool.isRequired,
   }).isRequired,
 };
 
