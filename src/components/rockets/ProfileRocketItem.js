c5f4e59ae14ccedff461c385d82b83b5ff08ceaf
Amrendra K <amrendrak.ind@gmail.com>
2023-06-01 13:18:42 +0100

    Update ProfileRocketItem.js

--- src/components/rockets/ProfileRocketItem.js
+++ src/components/rockets/ProfileRocketItem.js
@@ -1,30 +1,24 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import JoinButton from './JoinButton';
 import './Rocket.css';
 
-const ProfileRocketItem = (props) => {
-  const { rocket } = props;
+const ProfileRocketItem = ({ rocket }) => {
   const { id, name } = rocket;
 
   return (
-    <li
-      key={id}
-      className="profile-rocket-name"
-    >
+    <li className="profile-rocket-name">
       <h3>{name}</h3>
       <JoinButton rocket={rocket} />
     </li>
   );
 };
 
-export default ProfileRocketItem;
-
 ProfileRocketItem.propTypes = {
-  rocket:
-        PropTypes.objectOf(
-          {
-            id: PropTypes.number,
-            name: PropTypes.string,
-          },
-        ).isRequired,
+  rocket: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
 };
+
+export default ProfileRocketItem;
